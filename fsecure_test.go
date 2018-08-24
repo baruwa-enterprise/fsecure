c39fa5086289158e12a99a7dc8c843a144ad7ed7
Andrew Colin Kissa <andrew@topdog.za.net>
2018-08-24 16:28:34 +0200

    TST: Add tests

--- fsecure_test.go
+++ fsecure_test.go
@@ -8,3 +8,200 @@ Package fsecure Golang F-Secure client Library
 Fsecure - Golang F-Secure client Library
 */
 package fsecure
+
+import (
+	"fmt"
+	"os"
+	"testing"
+	"time"
+)
+
+const (
+	localSock = "/Users/andrew/fsav.sock"
+)
+
+func TestBasics(t *testing.T) {
+	address := os.Getenv("FSECURE_ADDRESS")
+	if address == "" {
+		address = localSock
+	}
+
+	if _, e := os.Stat(address); !os.IsNotExist(e) {
+		c, e := NewClient(address, 5*time.Second, 10*time.Second)
+		if e != nil {
+			t.Fatalf("An error should not be returned: %s", e)
+		}
+		defer c.Close()
+		if c.address != address {
+			t.Errorf("Got %q want %q", c.address, address)
+		}
+		if _, e = NewClient("fe80::879:d85f:f836:1b56%en1", 5*time.Second, 10*time.Second); e == nil {
+			t.Fatalf("An error should be returned")
+		}
+		expect := "The unix socket: fe80::879:d85f:f836:1b56%en1 does not exist"
+		if e.Error() != expect {
+			t.Errorf("Got %q want %q", e, expect)
+		}
+	}
+}
+
+func TestConnTimeOut(t *testing.T) {
+	address := os.Getenv("FSECURE_ADDRESS")
+	if address == "" {
+		address = localSock
+	}
+
+	if _, e := os.Stat(address); !os.IsNotExist(e) {
+		c, e := NewClient(address, 5*time.Second, 10*time.Second)
+		if e != nil {
+			t.Fatalf("An error should not be returned")
+		}
+		defer c.Close()
+		if c.connTimeout != 5*time.Second {
+			t.Errorf("The default conn timeout should be set")
+		}
+	}
+}
+
+func TestConnSleep(t *testing.T) {
+	address := os.Getenv("FSECURE_ADDRESS")
+	if address == "" {
+		address = localSock
+	}
+
+	if _, e := os.Stat(address); !os.IsNotExist(e) {
+		c, e := NewClient(address, 5*time.Second, 10*time.Second)
+		if e != nil {
+			t.Fatalf("An error should not be returned")
+		}
+		defer c.Close()
+		if c.connSleep != defaultSleep {
+			t.Errorf("The default conn sleep should be set")
+		}
+		expected := 2 * time.Second
+		c.SetConnSleep(expected)
+		if c.connSleep != expected {
+			t.Errorf("Calling c.SetConnSleep(%q) failed", expected)
+		}
+	}
+}
+
+func TestCmdTimeOut(t *testing.T) {
+	address := os.Getenv("FSECURE_ADDRESS")
+	if address == "" {
+		address = localSock
+	}
+
+	if _, e := os.Stat(address); !os.IsNotExist(e) {
+		c, e := NewClient(address, 5*time.Second, 10*time.Second)
+		if e != nil {
+			t.Fatalf("An error should not be returned")
+		}
+		defer c.Close()
+		if c.cmdTimeout != 10*time.Second {
+			t.Errorf("Expected %q got %q", 10*time.Second, c.cmdTimeout)
+		}
+		expected := 2 * time.Second
+		c.SetCmdTimeout(expected)
+		if c.cmdTimeout != expected {
+			t.Errorf("Calling c.SetCmdTimeout(%q) failed", expected)
+		}
+	}
+}
+
+func TestConnRetries(t *testing.T) {
+	address := os.Getenv("FSECURE_ADDRESS")
+	if address == "" {
+		address = localSock
+	}
+
+	if _, e := os.Stat(address); !os.IsNotExist(e) {
+		c, e := NewClient(address, 5*time.Second, 10*time.Second)
+		if e != nil {
+			t.Fatalf("An error should not be returned")
+		}
+		defer c.Close()
+		if c.connRetries != 0 {
+			t.Errorf("The default conn retries should be set")
+		}
+		c.SetConnRetries(2)
+		if c.connRetries != 2 {
+			t.Errorf("Calling c.SetConnRetries(%q) failed", 2)
+		}
+		c.SetConnRetries(-2)
+		if c.connRetries != 0 {
+			t.Errorf("Preventing negative values in c.SetConnRetries(%q) failed", -2)
+		}
+	}
+}
+
+func TestBasicError(t *testing.T) {
+	_, e := NewClient("", 5*time.Second, 10*time.Second)
+	if e == nil {
+		t.Fatalf("An error should not be returned")
+	}
+	expected := fmt.Sprintf("The unix socket: %s does not exist", FsavSock)
+	if e.Error() != expected {
+		t.Errorf("Got %q want %q", e, expected)
+	}
+}
+
+func TestScan(t *testing.T) {
+	address := os.Getenv("FSECURE_ADDRESS")
+	if address == "" {
+		address = localSock
+	}
+
+	if _, e := os.Stat(address); !os.IsNotExist(e) {
+		c, e := NewClient(address, 5*time.Second, 10*time.Second)
+		if e != nil {
+			t.Fatalf("An error should not be returned")
+		}
+		defer c.Close()
+		fn := "/var/spool/testfiles/eicar.tar.bz2"
+		r, e := c.Scan(fn)
+		if e != nil {
+			t.Fatalf("An error should not be returned")
+		}
+		if r.Filename != fn {
+			t.Errorf("r.Filename = %q, want %q", r.Filename, fn)
+		}
+		if r.ArchiveItem != "" {
+			t.Errorf("r.ArchiveItem = %q, want %q", r.ArchiveItem, "")
+		}
+		sig := "EICAR-Test-File (not a virus)"
+		if r.Signature != sig {
+			t.Errorf("r.Signature = %q, want %q", r.Signature, sig)
+		}
+		sts := "MIME_INFECTED"
+		if r.Status != sts {
+			t.Errorf("r.Status = %q, want %q", r.Status, sts)
+		}
+		if !r.Infected {
+			t.Errorf("r.Infected = %t, want %t", r.Infected, true)
+		}
+		fn = "/var/spool/testfiles/1fiU7D-00015D-74.eml"
+		r, e = c.Scan(fn)
+		if e != nil {
+			t.Fatalf("An error should not be returned")
+		}
+		if r.Filename != fn {
+			t.Errorf("r.Filename = %q, want %q", r.Filename, fn)
+		}
+		an := "[1] SWIFTMT103.exe"
+		if r.ArchiveItem != an {
+			t.Errorf("r.ArchiveItem = %q, want %q", r.ArchiveItem, an)
+		}
+		sig = "Trojan.GenericKD.40336677"
+		if r.Signature != sig {
+			t.Errorf("r.Signature = %q, want %q", r.Signature, sig)
+		}
+		sts = "ARCHIVE_INFECTED"
+		if r.Status != sts {
+			t.Errorf("r.Status = %q, want %q", r.Status, sts)
+		}
+		if !r.Infected {
+			t.Errorf("r.Infected = %t, want %t", r.Infected, true)
+		}
+	}
+}
