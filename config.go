228e469d45e3c6da383dd0598b15d8b69e925040
Andrew Colin Kissa <andrew@topdog.za.net>
2018-08-25 11:09:16 +0200

    FET: Add configurable fsav options

---
+++ config.go
@@ -0,0 +1,56 @@
+// Copyright (C) 2018 Andrew Colin Kissa <andrew@datopdog.io>
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at http://mozilla.org/MPL/2.0/.
+
+/*
+Package fsecure Golang F-Secure client Library
+Fsecure - Golang F-Secure client Library
+*/
+package fsecure
+
+import (
+	"fmt"
+)
+
+// Config holds Fsecure fsav options
+type Config struct {
+	Mime        BoolField
+	RiskWare    BoolField
+	StopOnFirst BoolField
+	Timeout     IntField
+	Archive     BoolField
+}
+
+// BoolField local adaptation of bool
+type BoolField bool
+
+func (b BoolField) String() (s string) {
+	s = "0"
+	if b {
+		s = "1"
+	}
+
+	return
+}
+
+// IntField local adaptation of int
+type IntField int
+
+func (i IntField) String() (s string) {
+	s = fmt.Sprintf("%d", i)
+
+	return
+}
+
+// NewConfig returns a new config object
+func NewConfig() (c *Config) {
+	c = &Config{
+		Mime:        true,
+		RiskWare:    true,
+		StopOnFirst: true,
+		Timeout:     60,
+		Archive:     true,
+	}
+	return
+}
--- fsecure.go
+++ fsecure.go
@@ -14,6 +14,7 @@ import (
 	"net"
 	"net/textproto"
 	"os"
+	"reflect"
 	"regexp"
 	"strings"
 	"sync"
@@ -55,6 +56,8 @@ type Client struct {
 	tc          *textproto.Conn
 	m           sync.Mutex
 	conn        net.Conn
+	// Config holds the fsav config options
+	Config *Config
 }
 
 // SetCmdTimeout sets the cmd timeout
@@ -75,6 +78,19 @@ func (c *Client) SetConnRetries(s int) {
 // duration in seconds
 func (c *Client) SetConnSleep(s time.Duration) {
 	c.connSleep = s
+}
+
+// SetOptions sets the fsav config options on the server
+func (c *Client) SetOptions() (err error) {
+	elm := reflect.ValueOf(c.Config).Elem()
+	for i := 0; i < elm.NumField(); i++ {
+		n := strings.ToUpper(elm.Type().Field(i).Name)
+		v := elm.Field(i).Interface()
+		if err = c.sendOpt(n, v); err != nil {
+			break
+		}
+	}
+	return
 }
 
 // Close closes the server connection
@@ -158,6 +174,41 @@ func (c *Client) dial() (conn net.Conn, err error) {
 	return
 }
 
+func (c *Client) sendOpt(n string, v interface{}) (err error) {
+	var id uint
+	var line string
+
+	id = c.tc.Next()
+	c.tc.StartRequest(id)
+	defer c.conn.SetDeadline(ZeroTime)
+
+	c.conn.SetDeadline(time.Now().Add(c.cmdTimeout))
+	if _, err = fmt.Fprintf(c.tc.W, "CONFIGURE\t%s\t%s\n", n, v); err != nil {
+		c.tc.EndRequest(id)
+		return
+	}
+
+	if err = c.tc.W.Flush(); err != nil {
+		c.tc.EndRequest(id)
+		return
+	}
+	c.tc.EndRequest(id)
+	c.tc.StartResponse(id)
+	defer c.tc.EndResponse(id)
+
+	c.conn.SetDeadline(time.Now().Add(c.cmdTimeout))
+	if line, err = c.tc.ReadLine(); err != nil {
+		return
+	}
+
+	if !strings.HasPrefix(line, "OK") {
+		err = fmt.Errorf("Set option: %s failed: %s", n, line)
+		return
+	}
+
+	return
+}
+
 // NewClient creates and returns a new instance of Client
 func NewClient(address string, connTimeOut, ioTimeOut time.Duration) (c *Client, err error) {
 	var line string
@@ -175,6 +226,7 @@ func NewClient(address string, connTimeOut, ioTimeOut time.Duration) (c *Client,
 		connTimeout: connTimeOut,
 		connSleep:   defaultSleep,
 		cmdTimeout:  ioTimeOut,
+		Config:      NewConfig(),
 	}
 
 	c.m.Lock()
@@ -215,6 +267,11 @@ func NewClient(address string, connTimeOut, ioTimeOut time.Duration) (c *Client,
 
 	if !strings.HasPrefix(line, "OK") {
 		err = fmt.Errorf("Protocol negotiation failed: %s", line)
+		c.tc.Close()
+		return
+	}
+
+	if err = c.SetOptions(); err != nil {
 		c.tc.Close()
 		return
 	}
