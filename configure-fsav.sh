c2266985eb5d6ddc7c58465dd698834a082ee46a
Andrew Colin Kissa <andrew@topdog.za.net>
2018-09-04 22:05:21 +0200

    TST: Support full travis tests

---
+++ configure-fsav.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -e
+sed -i -e 's/^odsFileScanInsideMIME\s\+[0-9]/odsFileScanInsideMIME 1/' \
+    -e 's/^odsFilePrimaryActionOnInfection\s\+[0-9]/odsFilePrimaryActionOnInfection 1/' \
+    -e 's/^odsFileSecondaryActionOnInfection\s\+[0-9]/odsFileSecondaryActionOnInfection 2/' \
+    -e 's/^odsAskQuestions\s\+[0-9]/odsAskQuestions 0/' \
+    -e 's/^odsFollowSymlinks\s\+[0-9]/odsFollowSymlinks 1/' \
+    -e 's/^daemonLogfileEnabled\s\+[0-9]/daemonLogfileEnabled 1/' \
+    -e 's/^daemonSocketMode\s\+[0-9]\+/daemonSocketMode 0666/' \
+    -e 's/^socketpathGroup\s\+\S\+/socketpathGroup Debian-exim/' /etc/opt/f-secure/fssp/fssp.conf
+cp -v /opt/f-secure/fssp/etc/fsavd /etc/init.d/
+chmod +x /etc/init.d/fsavd
+/etc/init.d/fsavd start
